a5d042422b0d56bbf34fde593cb2f7a648852009
PutriDewiKharisma123 <107486533+PutriDewiKharisma123@users.noreply.github.com>
2022-10-27 05:53:36 +0700

    Add files via upload

---
+++ mywebsite/deka/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+
+def putri(request):
+    return render(request,'putri.html')
---
+++ mywebsite/kharisma/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def kharisma(request):
+    return render(request,'deka.html')
